9d548451a2979c9fe24418b8b7346a00383ed2fb
navyabommisetty1998 <96114826+navyabommisetty1998@users.noreply.github.com>
2021-12-16 00:09:05 +0530

    initial commit

---
+++ navyasai.sql
@@ -0,0 +1,40 @@
+CREATE TABLE team(STDROLLN0 varchar(30),stdname varchar(30),
+subject varchar(30);
+insert into student(STDROLLN0,stdname,marks)
+values('1','navya','80');
+insert into stdinformation(stdrolln0,stdname,stdmarks)
+values('2','sai','90');
+insert into stdinformation(stdrolln0,stdname,stdmarks)
+values('3','charan','70');
+select*from student;
+CREATE TABLE stdinformation(stdrolln0 varchar(30),stdaddress varchar(30),
+stdsurname varchar(30));
+insert into stdinformation(stdrolln0,stdaddress,stdsurname)
+values('1','khammam','bommisetty');
+insert into stdinformation(stdrolln0,stdaddress,stdsurname)
+values('3','warangal','koduri');
+select*from stdinformation;
+insert into stdinformation(stdrolln0,stdaddress,stdsurname)
+values('2','hyderabad','donthamsetty');
+select*from stdinformation;
+select student.stdname,student.MARKS,
+stdinformation.stdaddress,stdinformation.stdsurname from  
+student  INNER JOIN stdinformation
+ ON 
+student.stdrolln0=stdinformation.stdrolln0;
+select student.stdname,student.MARKS,
+stdinformation.stdaddress,stdinformation.stdsurname from  
+student  LEFT JOIN stdinformation
+ ON 
+student.stdrolln0=stdinformation.stdrolln0;
+select student.stdname,student.MARKS,
+stdinformation.stdaddress,stdinformation.stdsurname from  
+student  RIGHT JOIN stdinformation
+ ON 
+student.stdrolln0=stdinformation.stdsurname;
+select student.stdname,student.MARKS,
+stdinformation.stdaddress,stdinformation.stdsurname from  
+student  full JOIN stdinformation
+ ON 
+student.stdrolln0=stdinformation.stdrolln0;
+
